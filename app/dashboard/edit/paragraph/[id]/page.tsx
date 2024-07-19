a5fabf61a141f0a0b530483aee45e8fd84605d58
octuniv <mayday010771@gmail.com>
2024-07-19 11:09:34 +0900

    1. delete main tag in paragraph page

--- app/dashboard/edit/paragraph/[id]/page.tsx
+++ app/dashboard/edit/paragraph/[id]/page.tsx
@@ -3,13 +3,9 @@ import EditForm from "@/app/ui/dashboard/edit/editParag";
 import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;
-    const paragraph = await fetchParagraphById(id);
-    if (!paragraph) notFound();
+  const id = params.id;
+  const paragraph = await fetchParagraphById(id);
+  if (!paragraph) notFound();
 
-    return (
-        <main>
-            <EditForm paragraph={paragraph}/>
-        </main>
-    );
+  return <EditForm paragraph={paragraph} />;
 }
