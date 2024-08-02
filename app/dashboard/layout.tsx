52a4cb6c80be74bc88e8ebc22e7f076215d0727f
octuniv <mayday010771@gmail.com>
2024-08-02 12:00:07 +0900

    1. delete transparent background and fix layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,13 +5,15 @@ import { Suspense } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-9">
-      <div className="self-start sticky top-0 col-span-2">
-        <Suspense fallback={<UserProfileSkeleton />}>
-          <UserProfile />
-        </Suspense>
+    <div className="mt-3 mx-3 bg-rose-50">
+      <div className="grid grid-cols-9 bg-rose-100">
+        <div className="self-start sticky top-0 col-span-2">
+          <Suspense fallback={<UserProfileSkeleton />}>
+            <UserProfile />
+          </Suspense>
+        </div>
+        <main className="col-span-7">{children}</main>
       </div>
-      <main className="col-span-7">{children}</main>
     </div>
   );
 }
